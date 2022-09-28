3cc7b5e264ccf070ccdf55c46f67424c15988476
Afzal Ali <72311983+AfzalAli231@users.noreply.github.com>
2022-09-28 07:13:47 +0500

    Update messageRoutes.js

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -3,7 +3,13 @@ const app = require( "express");
 
 const router = app.Router();
 
-router.post("/addmsg/", addMessage);
-router.post("/getmsg/", getMessages);
+router.post("/addmsg/", async (req, res, next) => {
+  const data = await addMessage(req.body, next);
+  res.json(data);
+});
+router.post("/getmsg/",  async (req, res, next) => {
+  const data = await getMessages(req.body, next);
+  res.json(data);
+}));
 
 module.exports = router;
