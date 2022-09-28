d1d885078f7573f21cdae5d476fd49533ca4a17c
Afzal Ali <72311983+AfzalAli231@users.noreply.github.com>
2022-09-28 07:15:05 +0500

    Update messageRoutes.js

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -10,6 +10,6 @@ router.post("/addmsg/", async (req, res, next) => {
 router.post("/getmsg/",  async (req, res, next) => {
   const data = await getMessages(req.body, next);
   res.json(data);
-}));
+});
 
 module.exports = router;
